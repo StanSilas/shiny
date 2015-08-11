24705aca15e367c25fa4b29c075134aceca16be4
StanSilas <viv4864@gmail.com>
2015-08-12 05:07:14 +0530

    Create ui.R
    this is the working modified code that is in accordance with the latest changes and the developments in various r packages , and exclusively in R Shiny and incubator

---
+++ ui.R
@@ -0,0 +1,48 @@
+#d1 new working
+#install.packages("shinyIncubator")
+library(shiny)
+#install shiny incubator using dev tools
+library(shinyIncubator)
+
+shinyUI(fluidPage(
+  
+  
+  headerPanel("Twitter Sentiment Analysis"),
+  
+  # Getting User Inputs
+  
+  sidebarPanel(
+    
+    wellPanel(
+      textInput("entity1", "Handle 1: ","#apple"),
+      textInput ("entity2","Handle 2: ","#google"),
+      HTML
+      ("<div style='font-size: 10px;font-weight: bold'> Enter the tweet tags that you want '#'</div>")
+      )  ,
+    wellPanel(
+      sliderInput("maxTweets","Number of recent tweets to use for analysis:",min=10,max=15000,value=20,step=1), # The max can, of course, be increased
+      actionButton(inputId='actb',icon =icon("twitter"), label="Analyze!")
+    )
+    
+    ),
+  
+  mainPanel(
+    
+  tabsetPanel(
+      
+      #Output from tab 4 ----So a box plot to see the distribution of scores of sentiments 
+      tabPanel("Sentiment Analysis", plotOutput("sentiboxplot"), HTML
+               ("<div> This plot shows the distribution of positive/negative sentiments about each entity. Note that tweets were cleaned before this analysis was performed. For each tweet, a net score of positive and negative sentiments are computed and this plot shows the distribution of scores.A higher sentiment score suggests more positive (or a less negative) discussion of that entity than the other.</div>"),
+               tableOutput("sentiheadtable"),tableOutput("sentitailtable"),id="test"),
+      
+      #Output from tab 5 - Word clouds - with some html tags
+      
+      tabPanel("Word Clouds",h2(textOutput("entity1wc")),plotOutput("entity1wcplot"),h2(textOutput("entity2wc")),plotOutput("entity2wcplot"))
+      
+      #Output from tabs 6 and 7, the raw tweets
+      #  new edit tabPanel("Entity 1 Raw tweets",tableOutput("tableentity1")),
+      #  new edit tabPanel("Entity 2 Raw tweets",tableOutput("tableentity2"))
+    )
+  )
+  
+))
